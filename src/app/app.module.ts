c9ea42a0df5ba29404e965ae6453b278ca8117dd
Elliott Migotti <elliottmigotti@gmail.com>
2022-07-04 17:03:19 +0200

    Test adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoggerComponent } from './components/logger/logger.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoggerComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
